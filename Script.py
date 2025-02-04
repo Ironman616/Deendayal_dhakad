b6db22064c9d836966bcf317b3cc7e5ca22ffccb
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2025-02-04 18:37:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -269,7 +269,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Movie_loverzz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/movie_loverzz>[Movie_Loverz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Movie_loverzz">{file_caption}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/Movie_loverzz>[Movie_Loverzz]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
